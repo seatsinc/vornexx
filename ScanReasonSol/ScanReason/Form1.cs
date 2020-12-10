8946afe17307f3d1e387a6fbe4576e3752e30151
Benson <jbenson@seatsinc.com>
2020-12-10 11:42:06 -0600

    Added a program to force the scanning of a down reason.

---
+++ ScanReasonSol/ScanReason/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using Newtonsoft.Json;
+
+namespace ScanReason
+{
+
+    
+
+
+    public partial class Form1 : Form
+    {
+
+        const string VORNEIP = "10.119.12.15";
+
+        SoundPlayer sp;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            System.IO.Stream str = Properties.Resources.ALARM;
+
+            sp = new SoundPlayer(str);
+
+            timer.Start();
+
+            sp.Play();
+
+        }
+
+        
+
+        private bool isMissingReason()
+        {
+
+            try
+            {
+                RestClient client = new RestClient();
+
+                // make requests to vorne
+                string requestPS = client.makeRequest("http://" + VORNEIP + "/api/v0/process_state/active", httpVerb.GET);
+
+
+
+                // deserialize objects
+                PSActive psActive = JsonConvert.DeserializeObject<PSActive>(requestPS);
+
+                if (psActive.data.process_state_reason == "down")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.ToString());
+                return false;
+            }
+        }
+
+
+        private void missingReasonTick()
+        {
+            this.WindowState = FormWindowState.Maximized;
+
+
+            this.BackColor = System.Drawing.Color.Red;
+
+            this.lblScan.Location = new Point((this.Width / 2) - (this.lblScan.Width / 2), (this.Height / 2) - this.lblScan.Height);
+            this.lblDownReason.Location = new Point((this.Width / 2) - (this.lblDownReason.Width / 2), this.Height / 2);
+
+            sp.Play();
+        }
+
+        private void allGoodTick()
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+
+            if (this.isMissingReason())
+            {
+                this.missingReasonTick();
+            }
+            else
+            {
+                this.allGoodTick();
+            }
+        }
+    }
+}
---
+++ ScanReasonSol/ScanReason/PSActive.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScanReason
+{
+    public struct Data
+    {
+        public bool active;
+        public double elapsed;
+        public bool enabled;
+        public string information_source;
+        public string last_toggled;
+        public string name;
+        public string performance_impact;
+        public int priority;
+        public string process_state_reason;
+        public string remaining;
+    }
+
+    public struct Meta
+    {
+        public string asset_id;
+        public string data_version;
+        public string device_id;
+    }
+
+
+    public class PSActive
+    {
+        public Data data;
+        public Meta meta;
+    }
+}
