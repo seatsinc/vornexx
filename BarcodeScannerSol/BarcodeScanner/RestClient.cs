84d84e5888374e95bc52e53420e87934c6bbc1ee
Benson <jbenson@seatsinc.com>
2021-03-19 16:45:43 -0500

    Made Vorne query asynchronous in barcode scanner program

--- BarcodeScannerSol/BarcodeScanner/RestClient.cs
+++ BarcodeScannerSol/BarcodeScanner/RestClient.cs
@@ -23,71 +23,66 @@ namespace BarcodeScanner
 
         
 
-        public string makeRequest(string endPoint, httpVerb httpMethod, byte[] postData = null)
+        public async void makeRequest(string endPoint, httpVerb httpMethod, byte[] postData = null)
         {
 
-            string strResponseValue = string.Empty;
-
-
-
-            try
+            await Task.Run(() =>
             {
-
-                System.Net.HttpWebRequest.DefaultWebProxy = null;
-                ServicePointManager.DefaultConnectionLimit = 20;
-                ServicePointManager.Expect100Continue = false;
-
-                
-
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(endPoint);
-
-
-                request.Method = httpMethod.ToString();
-
-                if (postData != null)
+                try
                 {
-                    using (Stream requestStream = request.GetRequestStream())
-                    {
-                        requestStream.Write(postData, 0, postData.Length);
 
-                        requestStream.Close();
+                    System.Net.HttpWebRequest.DefaultWebProxy = null;
+                    ServicePointManager.DefaultConnectionLimit = 20;
+                    ServicePointManager.Expect100Continue = false;
+
+
+
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(endPoint);
+
+
+                    request.Method = httpMethod.ToString();
+
+                    if (postData != null)
+                    {
+                        using (Stream requestStream = request.GetRequestStream())
+                        {
+                            requestStream.Write(postData, 0, postData.Length);
+
+                            requestStream.Close();
+                        }
+                    }
+
+
+                    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                    {
+
+                        // Process the response stream... (could be JSON, XML or HTML etc...)
+
+                        using (Stream responseStream = response.GetResponseStream())
+                        {
+                            if (responseStream != null)
+                            {
+                                using (StreamReader reader = new StreamReader(responseStream))
+                                {
+                                    string strResponseValue = reader.ReadToEnd();
+
+                                    reader.Close();
+                                } // end of StreamReader
+                            }
+
+                            responseStream.Close();
+                        } // end of ResponseStream
+
+                        response.Close();
+
                     }
                 }
-
-
-                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                catch (Exception exc)
                 {
-
-                    // Process the response stream... (could be JSON, XML or HTML etc...)
-
-                    using (Stream responseStream = response.GetResponseStream())
-                    {
-                        if (responseStream != null)
-                        {
-                            using (StreamReader reader = new StreamReader(responseStream))
-                            {
-                                strResponseValue = reader.ReadToEnd();
-
-                                reader.Close();
-                            } // end of StreamReader
-                        }
-
-                        responseStream.Close();
-                    } // end of ResponseStream
-
-                    response.Close();
-
+                    Console.WriteLine(exc.ToString());
                 }
-            }
-            catch (Exception exc)
-            {
-                strResponseValue = string.Empty;
-            }
 
-
-
-            return strResponseValue;
-
+            });
         }
     }
 
